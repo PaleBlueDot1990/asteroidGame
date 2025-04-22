b09d778dbf151f8108c726aef4967ec363ae454c
Bhuvnesh <bhuvnesh.bhuwan.golden@gmail.com>
2025-04-22 16:22:41 +0530

    moving up and down

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill('black')
         player.draw(screen)
         dt = clock.tick(60) / 1000
-        player.update(dt)
         pygame.display.flip()
         
     
